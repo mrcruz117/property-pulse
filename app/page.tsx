f01156173eb8cabd25f4b1e5c5c7703d843f537d
Michael Cruz <mcruz@aersale.com>
2024-08-30 17:03:50 -0400

    added routes and link

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,19 @@
+import Link from "next/link";
+
 const HomePage = () => {
   console.log("HomePage");
-  return <div className="text-2xl">HomePage</div>;
+  return (
+    <div>
+      <h1 className="text-3xl">HomePage</h1>
+      <Link
+        href={{
+          pathname: "/properties",
+        }}
+      >
+        Go to properties
+      </Link>
+    </div>
+  );
 };
 
 export default HomePage;
---
+++ app/properties/[id]/page.tsx
@@ -0,0 +1,5 @@
+const PropertyPage = () => {
+  return <div>Single property</div>;
+};
+
+export default PropertyPage;
