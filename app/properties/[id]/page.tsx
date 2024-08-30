4cd8466f515d3a0b762b214170eda3c5605e3380
Michael Cruz <mcruz@aersale.com>
2024-08-30 17:55:23 -0400

    cli component hooks

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,5 +1,25 @@
+"use client";
+import {
+  useRouter,
+  useParams,
+  useSearchParams,
+  usePathname,
+} from "next/navigation";
+
 const PropertyPage = () => {
-  return <div>Single property</div>;
+  const router = useRouter();
+  const { id } = useParams();
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+
+  return (
+    <div>
+      <button onClick={() => router.replace("/")}>Go home</button>
+      <div>Page: {id}</div>
+      <div>Search params: {searchParams.get("name")}</div>
+      <div>Pathname: {pathname}</div>
+    </div>
+  );
 };
 
 export default PropertyPage;
