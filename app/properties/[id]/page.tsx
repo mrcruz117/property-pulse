2c59718a36881a7197aefabdcc8c2dfc0f925599
Michael Cruz <mcruz@aersale.com>
2024-08-30 18:00:12 -0400

    params server comp

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,25 +1,5 @@
-"use client";
-import {
-  useRouter,
-  useParams,
-  useSearchParams,
-  usePathname,
-} from "next/navigation";
-
-const PropertyPage = () => {
-  const router = useRouter();
-  const { id } = useParams();
-  const searchParams = useSearchParams();
-  const pathname = usePathname();
-
-  return (
-    <div>
-      <button onClick={() => router.replace("/")}>Go home</button>
-      <div>Page: {id}</div>
-      <div>Search params: {searchParams.get("name")}</div>
-      <div>Pathname: {pathname}</div>
-    </div>
-  );
+const PropertyPage = ({ params }: { params: { id: string } }) => {
+  return <div>Property Page {params.id}</div>;
 };
 
 export default PropertyPage;
