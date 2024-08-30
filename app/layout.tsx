80a376ba18e338cac72d8122010cd05ec3f02a2f
Michael Cruz <mcruz@aersale.com>
2024-08-30 18:47:02 -0400

    navbar state

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,17 @@
 import "@/assets/styles/globals.css";
+import Navbar from "@/components/Navbar";
 
 export const metadata = {
   title: "Property Pulse",
   keywords: "real estate, property, home, house, rental",
-    description: "Find the perfect rental property for you",
+  description: "Find the perfect rental property for you",
 };
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html>
       <body>
+        <Navbar />
         <main>{children}</main>
       </body>
     </html>
