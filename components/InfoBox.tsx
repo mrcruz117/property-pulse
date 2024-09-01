85e2b777020f72e68ffc4eafb6aa71ebd17f52a7
mrcruz117 <mrcruz1989@gmail.com>
2024-08-31 21:40:55 -0400

    added home page components

--- components/InfoBox.tsx
+++ components/InfoBox.tsx
@@ -1,5 +1,33 @@
-const InfoBox = () => {
-  return <div>info box</div>;
+import Link from 'next/link';
+const InfoBox = ({
+  heading,
+  children,
+  backgroundColor = 'bg-gray-100',
+  textColor = 'text-gray-800',
+  buttonInfo,
+}: {
+  heading: string;
+  children: React.ReactNode;
+  backgroundColor?: string;
+  textColor?: string;
+  buttonInfo: {
+    text: string;
+    link: string;
+    backgroundColor: string;
+  };
+}) => {
+  return (
+    <div className={`${backgroundColor} p-6 rounded-lg shadow-md`}>
+      <h2 className={`${textColor} text-2xl font-bold`}>{heading}</h2>
+      <p className={`${textColor} mt-2 mb-4`}>{children}</p>
+      <Link
+        href={buttonInfo.link}
+        className={`${buttonInfo.backgroundColor} inline-block text-white rounded-lg px-4 py-2 hover:bg-gray-700`}
+      >
+        {buttonInfo.text}
+      </Link>
+    </div>
+  );
 };
 
 export default InfoBox;
