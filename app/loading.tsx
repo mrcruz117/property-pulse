54253dd99369d95f7a6a88077317629166767579
Michael Cruz <mcruz@aersale.com>
2024-09-04 09:48:09 -0400

    default loading page

---
+++ app/loading.tsx
@@ -0,0 +1,20 @@
+"use client";
+import { ClipLoader } from "react-spinners";
+
+const overRide = {
+  display: "block",
+  margin: "100px auto",
+};
+
+const LoadingPage = () => {
+  return (
+    <ClipLoader
+      color="#3B82f6"
+      cssOverride={overRide}
+      size={150}
+      aria-label="Loading Spinner"
+    />
+  );
+};
+
+export default LoadingPage;
