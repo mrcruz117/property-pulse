0009de8a2320785146d6d7b9fded4f848361e41e
mrcruz117 <mrcruz1989@gmail.com>
2024-10-02 21:23:19 -0400

    user and property model

---
+++ models/User.ts
@@ -0,0 +1,19 @@
+import { Schema, model, models } from 'mongoose';
+
+const UserSchema = new Schema(
+  {
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    username: { type: String, required: true },
+    image: { type: String },
+    bookmarks: [{ type: Schema.Types.ObjectId, ref: 'property' }],
+  },
+  { timestamps: true }
+);
+
+const User = models.User || model('User', UserSchema);
+
+export default User;
