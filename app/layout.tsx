3af5464187bdf754e7f1dc6fa524e225c252d846
mrcruz117 <mrcruz1989@gmail.com>
2024-10-06 17:13:54 -0400

    authprovider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,25 @@
-import "@/assets/styles/globals.css";
-import Footer from "@/components/Footer";
-import Navbar from "@/components/Navbar";
+import '@/assets/styles/globals.css';
+import Footer from '@/components/Footer';
+import Navbar from '@/components/Navbar';
+import AuthProvider from '@/components/AuthProvider';
 
 export const metadata = {
-  title: "Property Pulse",
-  keywords: "real estate, property, home, house, rental",
-  description: "Find the perfect rental property for you",
+  title: 'Property Pulse',
+  keywords: 'real estate, property, home, house, rental',
+  description: 'Find the perfect rental property for you',
 };
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <html>
-      <body>
-        <Navbar />
-        <main>{children}</main>
-        <Footer />
-      </body>
-    </html>
+    <AuthProvider>
+      <html>
+        <body>
+          <Navbar />
+          <main>{children}</main>
+          <Footer />
+        </body>
+      </html>
+    </AuthProvider>
   );
 };
 
