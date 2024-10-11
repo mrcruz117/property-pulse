91c030a7632987e0c629eaaa8a8c15ad0e683ade
mrcruz117 <mrcruz1989@gmail.com>
2024-10-10 20:12:19 -0400

    signin button

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -11,6 +11,7 @@ import {
   ClientSafeProvider,
   LiteralUnion,
   useSession,
+  signIn,
 } from 'next-auth/react';
 import { BuiltInProviderType } from 'next-auth/providers/index';
 
@@ -113,10 +114,17 @@ const Navbar = () => {
           {!session && (
             <div className='hidden md:block md:ml-6'>
               <div className='flex items-center'>
-                <button className='flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'>
-                  <FaGoogle className='text-white mr-2' />
-                  <span>Login or Register</span>
-                </button>
+                {providers &&
+                  Object.values(providers).map((provider) => (
+                    <button
+                      key={provider.id}
+                      onClick={() => signIn(provider.id)}
+                      className='flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'
+                    >
+                      <FaGoogle className='text-white mr-2' />
+                      <span>Login or Register</span>
+                    </button>
+                  ))}
               </div>
             </div>
           )}
