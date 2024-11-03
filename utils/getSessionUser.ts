eadd5835673933d91eaff0aa2e6dc8b587714984
mrcruz117 <mrcruz1989@gmail.com>
2024-11-02 23:08:41 -0400

    corrected getSessionUser

--- utils/getSessionUser.ts
+++ utils/getSessionUser.ts
@@ -2,19 +2,19 @@ import { getServerSession } from 'next-auth';
 import { authOptions } from '@/utils/authOptions';
 
 export const getSessionUser = async () => {
-  try {
-    const session = await getServerSession(authOptions);
+  // try {
+  const session = await getServerSession(authOptions);
 
-    if (!session?.user) {
-      return null;
-    }
-
-    return {
-      // userId: session.user.id || '',
-      user: session.user,
-    };
-  } catch (error) {
-    console.error('Error fetching session user:', error);
+  if (!session?.user) {
     return null;
   }
+
+  return {
+    // userId: session.user.id || '',
+    user: session.user,
+  };
+  // } catch (error) {
+  //   console.error('Error fetching session user:', error);
+  //   return null;
+  // }
 };
