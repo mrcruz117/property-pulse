1eb97f0bce2d37d9273b6ce859e143cdd16f7440
mrcruz117 <mrcruz1989@gmail.com>
2024-11-17 20:18:50 -0500

    update property page

--- app/properties/[id]/edit/page.tsx
+++ app/properties/[id]/edit/page.tsx
@@ -1,11 +1,25 @@
 import PropertyEditForm from '@/components/PropertyEditForm';
+import connectDb from '@/config/database';
+import Property, { IProperty } from '@/models/Property';
+import { convertToObject } from '@/utils/convertToObject';
 
-const PropertyEditPage = () => {
+const PropertyEditPage = async ({ params }: { params: { id: string } }) => {
+  await connectDb();
+  const propertyDoc = await Property.findById(params.id).lean();
+  const property = convertToObject(propertyDoc) as IProperty;
+
+  if (!property) {
+    return (
+      <h1 className='text-center text-2xl font-bold mt-10'>
+        Property not found
+      </h1>
+    );
+  }
   return (
     <section className='bg-blue-50'>
       <div className='container m-auto max-w-2xl py-24'>
         <div className='bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0'>
-          <PropertyEditForm />
+          <PropertyEditForm property={property} />
         </div>
       </div>
     </section>
