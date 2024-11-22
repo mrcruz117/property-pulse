9473fdd10a1d02555b97708ebbedc1878ad155ea
mrcruz117 <mrcruz1989@gmail.com>
2024-11-21 20:31:59 -0500

    bookmarkProperty action

---
+++ app/actions/bookmarkProperty.ts
@@ -0,0 +1,40 @@
+'use server';
+
+import connectDb from '@/config/database';
+import User from '@/models/User';
+import { getSessionUser } from '@/utils/getSessionUser';
+import { revalidatePath } from 'next/cache';
+
+export async function bookmarkProperty(propertyId: string) {
+  await connectDb();
+  const sessionUser = await getSessionUser();
+
+  if (!sessionUser || !sessionUser.userId) {
+    throw new Error('User not authenticated');
+  }
+
+  const { userId } = sessionUser;
+  const user = await User.findById(userId);
+  let isBookmarked = user?.bookmarks.includes(propertyId);
+  if (!user) {
+    throw new Error('User not found');
+  }
+
+  let message;
+
+  if (isBookmarked) {
+    // remove if bookmarked
+    user.bookmarks.pull(propertyId);
+    message = 'Property removed from bookmarks';
+    isBookmarked = false;
+  } else {
+    // if not then add
+    user.bookmarks.push(propertyId);
+    message = 'Property added to bookmarks';
+    isBookmarked = true;
+  }
+
+  await user.save();
+  revalidatePath('/properties/saved', 'page');
+  return { message, isBookmarked };
+}
