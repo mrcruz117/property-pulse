c2ef990603799d6bd78bc2b6b02fcd727271dba8
mrcruz117 <mrcruz1989@gmail.com>
2024-11-21 20:17:17 -0500

    sidebar items

--- components/BookmarkButton.tsx
+++ components/BookmarkButton.tsx
@@ -1,7 +1,12 @@
 import { IProperty } from '@/models/Property';
+import { FaBookmark } from 'react-icons/fa';
 
 const BookmarkButton = ({ property }: { property: IProperty }) => {
-  return <button>Bookmark</button>;
+  return (
+    <button className='bg-blue-500 hover:bg-blue-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center'>
+      <FaBookmark className='mr-2'></FaBookmark> Bookmark Property
+    </button>
+  );
 };
 
 export default BookmarkButton;
