70f68d0a8f25ecffef8c41a004fd6bfa5bbf19d7
mrcruz117 <mrcruz1989@gmail.com>
2024-11-23 17:59:40 -0500

    search page

--- app/properties/search-results/page.tsx
+++ app/properties/search-results/page.tsx
@@ -1,8 +1,11 @@
+import PropertyCard from '@/components/PropertyCard';
+import PropertySearchForm from '@/components/PropertySearchForm';
 import connectDb from '@/config/database';
 import Property, { IProperty } from '@/models/Property';
 import { convertToObject } from '@/utils/convertToObject';
 import { FilterQuery } from 'mongoose';
-
+import Link from 'next/link';
+import { FaArrowAltCircleLeft } from 'react-icons/fa';
 const SearchResultsPage = async ({
   searchParams: { location, propertyType },
 }: {
@@ -27,8 +30,36 @@ const SearchResultsPage = async ({
   const propertiesQueryRes = await Property.find(query).lean();
   const properties = propertiesQueryRes.map((doc) => convertToObject(doc));
 
-  console.log(properties);
-  return <div>SearchResultsPage</div>;
+  return (
+    <>
+      <section className='bg-blue-700 py-4'>
+        <div className='max-w-7xl mx-auto px-4 flex flex-col items-start sm:px-6 lg:px-8'>
+          <PropertySearchForm />
+        </div>
+      </section>
+      <section className='px-4 py-6'>
+        <div className='container-xl lg:container m-auto px-4 py-6'>
+          <Link
+            href='/properties'
+            className='flex items-center text-blue-500 hover:underline mb-3'
+          >
+            <FaArrowAltCircleLeft className='mr-2 mb-1' />
+            Back to Properties
+          </Link>
+          <h1 className='text-2xl mb-4'>Search Results</h1>
+          {properties.length === 0 ? (
+            <p>No properties found</p>
+          ) : (
+            <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+              {properties.map((property) => (
+                <PropertyCard key={property._id} property={property} />
+              ))}
+            </div>
+          )}
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default SearchResultsPage;
