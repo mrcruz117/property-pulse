90c2547a94221cc3b2d9934c24c15fc11668d8ea
mrcruz117 <mrcruz1989@gmail.com>
2024-11-23 19:52:21 -0500

    message action

---
+++ app/actions/addMessage.ts
@@ -0,0 +1,36 @@
+'use server';
+
+import connectDb from '@/config/database';
+import Message from '@/models/Message';
+import { getSessionUser } from '@/utils/getSessionUser';
+
+export async function addMessage(formData: FormData) {
+  await connectDb();
+  const sessionUser = await getSessionUser();
+
+  if (!sessionUser) {
+    throw new Error('User not authenticated');
+  }
+
+  const { user } = sessionUser;
+  const userId = user?.id;
+
+  const recipient = formData.get('recipient');
+
+  if (userId === recipient) {
+    throw new Error('You cannot send a message to yourself');
+  }
+  const newMessage = new Message({
+    sender: userId,
+    recipient,
+    property: formData.get('property'),
+    name: formData.get('name'),
+    email: formData.get('email'),
+    phone: formData.get('phone'),
+    body: formData.get('body'),
+  });
+
+  await newMessage.save();
+
+  return { success: true };
+}
